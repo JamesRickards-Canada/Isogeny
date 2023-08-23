e9aab6d949411cd4117be83a4c7f11c589998fdc
JamesRickards-Canada <james.rickards@colorado.edu>
2023-08-23 10:00:40 -0600

    Added method to retrieve the sparse matrix from file.

--- eigenvalues.py
+++ eigenvalues.py
@@ -4,7 +4,22 @@ import numpy as np
 
 def secondeval(p, l):
 	drc = np.loadtxt("./scipy_adj/"+str(p)+"_"+str(l)+".dat", dtype=int)
-	nrows = drc[1][-1] + 1
+	if type(drc) == type(drc[1]):
+		nrows = drc[1][-1] + 1
+	else:#Happens when only 1 vertex
+		return
+	if nrows <= 3:#Too small for sparse array.
+		return
 	M = csr_array((drc[0], (drc[1], drc[2])), dtype='d', shape = (nrows, nrows))
 	a, _ = eigs(M, k = 2, which="LR", return_eigenvectors = False)
 	return np.real(a)
+	
+def sparseadjmtx(p, l):
+	drc = np.loadtxt("./scipy_adj/"+str(p)+"_"+str(l)+".dat", dtype=int)
+	if type(drc) == type(drc[1]):
+		nrows = drc[1][-1] + 1
+	else:#Happens when only 1 vertex
+		return
+	if nrows <= 3:#Too small for sparse array.
+		return
+	return csr_array((drc[0], (drc[1], drc[2])), dtype='d', shape = (nrows, nrows))
