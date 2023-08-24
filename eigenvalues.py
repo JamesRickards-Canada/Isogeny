8d1721af16f70056b0485c8d67411b2c42b6d447
JamesRickards-Canada <james.rickards@colorado.edu>
2023-08-23 22:33:58 -0600

    Updated eigenvalues.py to allow for L-isogeny graphs, as well as computing the second largest eigenvalue in absolute value.

--- eigenvalues.py
+++ eigenvalues.py
@@ -2,9 +2,20 @@ from scipy.sparse.linalg import eigs
 from scipy.sparse import csr_array
 from scipy.linalg import eig
 import numpy as np
-	
+
+#All of these computations assume that ssl_graph_scipy(p, l) has already been called. l can be an array.
+
+#Returns the adjacency matrix as a sparse array
 def sparseadjmtx(p, l):
-	drc = np.loadtxt("./scipy_adj/"+str(p)+"_"+str(l)+".dat", dtype=int)
+	filename = "./scipy_adj/" + str(p) + "_"
+	if type(l) == type(1):
+		filename = filename + str(l)+ ".dat"
+	else:
+		filename = filename + str(l[0])
+		for lval in l[1:]:
+			filename = filename + "-" + str(lval)
+		filename = filename + ".dat"
+	drc = np.loadtxt(filename, dtype=int)
 	if type(drc) == type(drc[1]):
 		nrows = drc[1][-1] + 1
 	else:#Happens when only 1 vertex
@@ -13,11 +24,24 @@ def sparseadjmtx(p, l):
 		return
 	return csr_array((drc[0], (drc[1], drc[2])), dtype='d', shape = (nrows, nrows))
 	
+#Returns the second largest eigenvalue of the graph
 def secondeval(p, l):
 	M = sparseadjmtx(p, l)
-	a, _ = eigs(M, k = 2, which="LR", return_eigenvectors = False)
+	a, _ = eigs(M, k = 2, which = "LR", return_eigenvectors = False)
 	return np.real(a)
 
+#Returns the second largest eigenvalue in terms of absolute value of the graph
+def secondabseval(p, l):
+	M = sparseadjmtx(p, l)
+	a, _ = eigs(M, k = 2, which = "LR", return_eigenvectors = False)
+	b = eigs(M, k = 1, which = "SR", return_eigenvectors = False)[0]
+	a = np.abs(np.real(a))
+	b = np.abs(np.real(b))
+	if a < b:
+		return b
+	return a
+
+#Returns all the eigenvalues of the graph
 def allevals(p, l):
 	M = sparseadjmtx(p, l)
 	ev = eig(M.toarray(), right=False)
